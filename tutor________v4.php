00325c6214a742da998a32c588dc2818a9686682
samir aoulad amar <144660881+samir20-23@users.noreply.github.com>
2024-09-18 13:03:57 +0100

    Update tutor________v4.php

--- tutor________v4.php
+++ tutor________v4.php
@@ -1,57 +1,24 @@
-
 <?php
-class Utilisateur {
-    private $nom;
-    private $prenom;
-    private $email;
 
-    public function __construct($nom, $prenom, $email) {
-        $this->nom = $nom;
-        $this->prenom = $prenom;
-        $this->email = $email;
-    }
-
-    // Getters
-    public function getNom() {
-        return  $this->nom;
-    }
-
-    public function getPrenom() {
-        return  $this->prenom;
-    }
-
-    public function getEmail() {
-        return  $this->email;
-    }
-
-    // Setters
-    public function setNom($nom) {
-        $this->nom = $nom;
-    }
-
-    public function setPrenom($prenom) {
-        $this->prenom = $prenom;
-    }
-
-    public function setEmail($email) {
-        $this->email = $email;
+class An{
+    public function an(){
+        echo "an 1 !";
     }
 }
-function getInput($ev){
-    echo $ev;
-    return trim(fgetc(STDIN));
+class An2 extends An{
+    public function an(){
+        echo "an 2 ! ";
+    }
 }
-$nom = getInput('enter nom :');
-$email = getInput('enter Email :');
-$pnom= getInput('enter prenom :');
+class An3 extends An2 {
+    public function an(){
+        echo "an 3 !";
+    }
+}
+$arr = [new An(), new An2(),new An3()];
 
-$r = new Utilisateur($nom,$pnom,$email);
-$r->setPrenom($pnom);
-$r->setEmail($email);
-$r->setNom($nom);
+foreach($arr as $arr){
+    $c = $arr;
+    $c->an();
 
-
-$r->getPrenom();
-$r->getEmail();
-$r->getNom();
-?>
+}
