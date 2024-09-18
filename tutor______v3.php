0580c934f39021a1ba0af45c01865a9e99378695
samir aoulad amar <144660881+samir20-23@users.noreply.github.com>
2024-09-18 12:48:49 +0100

    Update tutor______v3.php

--- tutor______v3.php
+++ tutor______v3.php
@@ -1,50 +1,33 @@
-<?php
-class Animal
-{
-  public $name;
-  public $age;
+<?php 
+class Animal {
+    public $nom;
+    public $age;
 
-  // Constructor for Animal class
-  public function __construct($name, $age)
-  {
-    $this->name = $name;
-    $this->age = $age;
-  }
+    public function manger() {
+        echo "Je mange.";
+    }
+}
 
-  public function eat()
-  {
-    echo "I'm eating.\n";
+class Chat extends Animal {
+    public function miauler() {
+        echo "Miaou !";
+    }
+}
+
+
+// 
+class Chien extends Animal {
+  public function faireDuBruit() {
+      echo "Ouaf !";
   }
 }
 
-class Cat extends Animal
-{
-  public $breed;
+ 
 
-  public function __construct($name, $age, $breed)
-  {
-    parent::__construct($name, $age);
-    $this->breed = $breed;
-  }
+ $c = new Animal();
+ $cl = new Chat();
+ $class = new Chien(); 
+ $c->manger();
+ $cl->miauler();
+ $class->faireDuBruit();
 
-  public function meow()
-  {
-    echo "Miaou!\n";
-  }
-}
-
-class Dog extends Animal
-{
-  public function makeNoise()
-  {
-    echo "Ouaf!\n";
-  }
-}
-
-$myCat = new Cat("Rawana", 2, "ma8rabi");
-$myCat->meow();
-$myCat->eat();
-
-$myDog = new Dog("Walo", 4);
-$myDog->makeNoise();
-$myDog->eat();
