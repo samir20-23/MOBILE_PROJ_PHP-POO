a896257f80a22df84f6ff9877e623e039035b26a
samir aoulad amar <144660881+samir20-23@users.noreply.github.com>
2024-09-20 02:05:41 +0100

    Add files via upload

---
+++ tutor________________v8.php
@@ -0,0 +1,37 @@
+<?php
+
+class Livre {
+    public $titre;
+    public $isbn;
+    public $auteurs;  
+
+    public function __construct($titre, $isbn, $auteurs) {
+        $this->titre = $titre;
+        $this->isbn = $isbn;
+        $this->auteurs = $auteurs;
+    }
+}
+
+class Auteur {
+    public $nom;
+    public $prenom;
+
+    public function __construct($nom, $prenom) {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+    }
+}
+
+ 
+$auteur1 = new Auteur("Saint-Exupéry", "Antoine de");
+$livre1 = new Livre("Le Petit Prince", "9782266000016", [$auteur1]);
+
+ 
+$json = json_encode($livre1, JSON_PRETTY_PRINT);
+file_put_contents('ma_bibliotheque.json', $json);
+ 
+$json = file_get_contents('ma_bibliotheque.json');
+$livreLu = json_decode($json);
+
+ 
+echo $livreLu->titre;  
