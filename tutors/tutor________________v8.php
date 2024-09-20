a66b5cb417aa114eb7d4b3d7a33e8eb4f106428b
samir aoulad amar <144660881+samir20-23@users.noreply.github.com>
2024-09-20 09:32:29 +0100

    Update tutor________________v8.php

--- tutors/tutor________________v8.php
+++ tutors/tutor________________v8.php
@@ -1,37 +1,9 @@
 <?php
+echo 'hello';
 
-class Livre {
-    public $titre;
-    public $isbn;
-    public $auteurs;  
 
-    public function __construct($titre, $isbn, $auteurs) {
-        $this->titre = $titre;
-        $this->isbn = $isbn;
-        $this->auteurs = $auteurs;
-    }
-}
+//write in  terminal
+//cd /you_file/file.php
+//php -S localhost:8000
+//go to web write http://localhost:8000/youfile.php
 
-class Auteur {
-    public $nom;
-    public $prenom;
-
-    public function __construct($nom, $prenom) {
-        $this->nom = $nom;
-        $this->prenom = $prenom;
-    }
-}
-
- 
-$auteur1 = new Auteur("Saint-Exupéry", "Antoine de");
-$livre1 = new Livre("Le Petit Prince", "9782266000016", [$auteur1]);
-
- 
-$json = json_encode($livre1, JSON_PRETTY_PRINT);
-file_put_contents('data.json', $json);
- 
-$json = file_get_contents('data.json');
-$livreLu = json_decode($json);
-
- 
-echo $livreLu->titre;  
